41d01e2c954e03bf46799897a6b4540c5ed03f95
Daniela Rueda <daaniela0920@gmail.com>
2021-05-06 13:14:24 -0500

    Inicio Responsive

--- src/components/Inicio.vue
+++ src/components/Inicio.vue
@@ -1,37 +1,41 @@
 <template>
-  <div
-    class="container"
-    
-    :style="cssProps"
-  >
-    <BarraNavegacion />
-    <v-card class="pa-5 mx-auto" min-width="600">
-      <form>
-        <v-text-field
-          v-model="Restaurante"
-          label="Restaurante o Comida"
-          color="black"
-          outlined
-          dense
-        >
-          <v-icon slot="prepend" color="#FF4B4B">mdi-store</v-icon>
-        </v-text-field>
-        <v-text-field
-          v-model="Ubicacion"
-          label="Ubicación"
-          color="black"
-          outlined
-          dense
-        >
-          <v-icon slot="prepend" color="#FF4B4B">mdi-map-marker</v-icon>
-        </v-text-field>
-        <router-link to="/Principal">
-          <v-btn class="mr-4" color="#FF4B4B" elevation="2" block dark>
-            Buscar
-          </v-btn>
-        </router-link>
-      </form>
-    </v-card>
+  <div class="container" :style="cssProps">
+    <b-container>
+      <b-row>
+        <b-col></b-col>
+        <b-col cols="6">
+          <BarraNavegacion />
+          <v-card class="pa-5 mx-auto">
+            <form>
+              <v-text-field
+                v-model="Restaurante"
+                label="Restaurante o Comida"
+                color="black"
+                outlined
+                dense
+              >
+                <v-icon slot="prepend" color="#FF4B4B">mdi-store</v-icon>
+              </v-text-field>
+              <v-text-field
+                v-model="Ubicacion"
+                label="Ubicación"
+                color="black"
+                outlined
+                dense
+              >
+                <v-icon slot="prepend" color="#FF4B4B">mdi-map-marker</v-icon>
+              </v-text-field>
+              <router-link to="/Principal">
+                <v-btn class="mr-4" color="#FF4B4B" elevation="2" block dark>
+                  Buscar
+                </v-btn>
+              </router-link>
+            </form>
+          </v-card>
+        </b-col>
+        <b-col></b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 <script>
@@ -42,13 +46,12 @@ export default {
   },
   data() {
     return {
-      cssProps:{
-        backgroundImage: `url(${require('@/assets/fondo.jpeg')})`
-      }
+      cssProps: {
+        backgroundImage: `url(${require("@/assets/datafood_imagen_principal.jpg")})`,
+      },
     };
   },
-  methods: {
-  },
+  methods: {},
 };
 </script>
 <style scoped>
@@ -58,9 +61,12 @@ export default {
   align-items: center;
   height: 100%;
   background-position: center;
-  background-size: 150%;
 }
-.container, .container-sm, .container-md, .container-lg, .container-xl {
-    max-width: 1350px;
+.container,
+.container-sm,
+.container-md,
+.container-lg,
+.container-xl {
+  max-width: 1350px;
 }
 </style>
